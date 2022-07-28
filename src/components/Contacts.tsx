58417f5dde8fd6144d06d11e88ffda74e3433a58
Mikhail Romanov <w38@bk.ru>
2022-07-28 23:43:46 +0300

    contacts section edited

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -4,17 +4,13 @@ import classes from './Content.module.css'
 //использование тега form по умолчанию приводит к перерисовке компонента
 const Contacts=()=> {
     return (
-        <div className={classes.skills} id="contacts">
+        <div className={classes.contacts} id="contacts">
 <h2>Contacts</h2>
-            <div className={classes.formContainer}>
-
-
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea></textarea>
-                <button type={"submit"} >Submit</button>
-
+            <div className={classes.contacts}>
+                <p>w38@bk.ru</p>
+                <p>916-333-55-65</p>
             </div>
+
         </div>
     );
 }
